a0dc29791fee25f8a8d852fe7b3c37007f7a7c2e
ajpayzant <ajpayzant@icloud.com>
2026-05-31 14:49:34 -0700

    Update projection_app.py

--- projection_app.py
+++ projection_app.py
@@ -6,7 +6,7 @@ Entry point. Run with:  streamlit run projection_app.py
 import streamlit as st
 
 st.set_page_config(
-    page_title="PLL Projection Engine",
+    page_title="PLL Projections",
     page_icon="🥍",
     layout="wide",
     initial_sidebar_state="expanded",
